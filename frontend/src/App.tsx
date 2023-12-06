dac13f1d3069622388efb6289bdce3f015c01b09
Magnus Byrkjeland <magnus.e.b@hotmail.com>
2023-12-06 10:29:55 +0000

    layout

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,11 +1,11 @@
 import "./App.css";
+import LeftBar from "./components/LeftBar";
 import Map from "./components/Map";
 
 function App() {
 	return (
 		<div className="container">
-			<h1>Ajerlines &#128745;</h1>
-			<h2>Where is Captain Olsen today?</h2>
+			<LeftBar />
 			<Map />
 		</div>
 	);
--- frontend/src/components/Map.tsx
+++ frontend/src/components/Map.tsx
@@ -2,24 +2,41 @@ import {
 	ComposableMap,
 	Geographies,
 	Geography,
+	Graticule,
 	Marker,
+	Sphere,
+	ZoomableGroup,
 } from "react-simple-maps";
+import "./Map.css";
 
+type Coordinates = [number, number];
 const geoUrl = "https://cdn.jsdelivr.net/npm/world-atlas@2/countries-110m.json";
 
 export default function MapChart() {
+	const coordinates: Coordinates = [-15.57808, 27.8282];
 	return (
-		<ComposableMap>
-			<Geographies geography={geoUrl}>
-				{({ geographies }) =>
-					geographies.map((geo) => (
-						<Geography key={geo.rsmKey} geography={geo} />
-					))
-				}
-			</Geographies>
-			<Marker coordinates={[-15.57808, 27.8282]}>
-				<circle r={2} fill="#F53" />
-			</Marker>
-		</ComposableMap>
+		<div className="map-container">
+			<ComposableMap
+				stroke="#00FF00"
+				strokeWidth={0.2}
+				fill="#666"
+				projection="geoMercator"
+			>
+				<ZoomableGroup zoom={5} center={coordinates}>
+					<Sphere stroke="#000" strokeWidth={0.3} id={""} fill="transparent" />
+					<Graticule stroke="#000" strokeWidth={0.1} />
+					<Geographies geography={geoUrl} fill="#335533">
+						{({ geographies }) =>
+							geographies.map((geo) => (
+								<Geography key={geo.rsmKey} geography={geo} />
+							))
+						}
+					</Geographies>
+					<Marker coordinates={coordinates}>
+						<circle r={1} fill="#F53" />
+					</Marker>
+				</ZoomableGroup>
+			</ComposableMap>
+		</div>
 	);
 }
